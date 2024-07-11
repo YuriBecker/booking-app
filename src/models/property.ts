0375b77ada32f3baf99735f4b915f16b3cbac941
Yuri Becker <becker.yr@gmail.com>
2024-07-11 19:45:12 -0300

    feat: add property model

---
+++ src/models/property.ts
@@ -0,0 +1,37 @@
+export type Property = {
+  id: string;
+  category: "room" | "entire-home";
+  numberOfBeds: number;
+  numberOfBedrooms: number;
+  numberOfBathrooms: number;
+  price: Price;
+  location: Location;
+  capacity: Capacity;
+  reviews: Reviews;
+  title: string;
+  description: string;
+  images: string[];
+};
+
+export type Capacity = {
+  adults: number;
+  children: number;
+};
+
+export type Location = {
+  city: string;
+  state: string;
+  countryCode: string;
+};
+
+export type Price = {
+  perNight: number;
+  hasPromotion: boolean;
+  promotionalPricePerNight: number | null;
+  cleaningFee: number | null;
+};
+
+export type Reviews = {
+  totalScore: number;
+  reviewsCount: number;
+};
