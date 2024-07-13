d833fb225b577df5f6cb0cf287b0a32654b5f3aa
Yuri Becker <becker.yr@gmail.com>
2024-07-12 22:12:59 -0300

    feat: update button and loader layout

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,4 +1,5 @@
-import { cn } from "@/utils/tailwind";
+import Lottie from "react-lottie";
+import animationData from "../../../public/loader-animation.json";
 
 type Props = React.SVGProps<SVGSVGElement> & {
   size?: number;
@@ -6,27 +7,23 @@ type Props = React.SVGProps<SVGSVGElement> & {
   fixed?: boolean;
 };
 
-export const Loader = ({ className, size = 48, fixed }: Props) => {
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+export const Loader = ({ size = 120, fixed }: Props) => {
   const containerClass = fixed
     ? "fixed inset-0 flex items-center justify-center"
     : "";
 
   return (
     <div className={containerClass}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={size}
-        height={size}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className={cn("animate-spin", "text-secondary", className)}
-      >
-        <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-      </svg>{" "}
+      <Lottie options={defaultOptions} height={size} width={size} />
     </div>
   );
 };
