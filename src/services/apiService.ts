640b35bb4a7ae0d9946ae2071c3745f09bdea3d0
Yuri Becker <becker.yr@gmail.com>
2024-07-12 22:03:23 -0300

    feat: add API service and env file

---
+++ src/services/apiService.ts
@@ -0,0 +1,36 @@
+import { PaginatedData } from "@/models/pagination";
+import { Property } from "@/models/property";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const API_URL = import.meta.env.VITE_API_URL;
+const PROPERTIES_PER_PAGE = 15;
+
+type GetPropertiesQuery = {
+  page: number;
+  sort?: string;
+};
+
+export const apiService = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_URL,
+  }),
+  endpoints: (builder) => ({
+    getProperties: builder.query<PaginatedData<Property[]>, GetPropertiesQuery>(
+      {
+        query: ({ page, sort }) => ({
+          url: "properties",
+          params: {
+            _page: page,
+            _per_page: PROPERTIES_PER_PAGE,
+            _sort: sort,
+          },
+        }),
+      }
+    ),
+    getProperty: builder.query<Property, string>({
+      query: (id) => `properties/${id}`,
+    }),
+  }),
+});
+
+export const { useGetPropertiesQuery, useGetPropertyQuery } = apiService;
