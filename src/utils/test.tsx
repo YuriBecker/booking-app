1aed999b54a5f3f42d5c6d3712f50f945b2fc7d3
Yuri Becker <becker.yr@gmail.com>
2024-07-21 12:41:24 -0300

    feat: add renderWithProviders test util

---
+++ src/utils/test.tsx
@@ -0,0 +1,31 @@
+import React, { PropsWithChildren } from "react";
+import { render } from "@testing-library/react";
+import type { RenderOptions } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { setupStore } from "@/store";
+import type { AppStore, RootState } from "@/store";
+import { MemoryRouter } from "react-router-dom";
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, "wrapper"> {
+  preloadedState?: Partial<RootState>;
+  store?: AppStore;
+}
+
+export const renderWithProviders = (
+  ui: React.ReactElement,
+  {
+    preloadedState,
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) => {
+  function AllTheProviders({ children }: PropsWithChildren): JSX.Element {
+    return (
+      <Provider store={store}>
+        <MemoryRouter>{children}</MemoryRouter>
+      </Provider>
+    );
+  }
+
+  return render(ui, { wrapper: AllTheProviders, ...renderOptions });
+};
